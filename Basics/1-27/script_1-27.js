b831b46d4c73cb47c7e8b7afc3c78b1863e23de6
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-10-20 21:29:45 +0300

    Задачи 18-27

---
+++ Basics/1-27/script_1-27.js
@@ -0,0 +1,85 @@
+// 1 - 11
+"use strict";
+// dfd
+/* dfsdf
+dfsdf */
+
+// 13
+// let num = 123;
+// alert(num);
+
+
+
+// 15
+// let a;
+// a = 10;
+// alert(a);
+
+// a = 20;
+// alert(a);
+
+
+
+
+// 17
+// let a = 1 + 2 + 3;
+// alert(a);
+
+
+
+// Задание 18 №1
+// let a1 = 10;
+// let b1 = 2;
+
+// alert(a1+b1)
+// alert(a1-b1)
+// alert(a1*b1)
+// alert(a1/b1)
+
+// Задание 18 №2
+// let c = 10;
+// let d = 5;
+// let result= c + d;
+
+// alert(result)
+
+// Задание 18 №3
+// let a3 = 1;
+// let b3 = 2;
+// let c3 = 3;
+
+// alert(a3+b3+c3)
+
+// Задание 18 №4
+// let a4 = 10;
+// let b4 = 5;
+// let c4 = a4 - b4;
+// let d4 = 7;
+// let result4 = c4 + d4;
+
+// alert(result4);
+
+// Задание 22 №1
+// let a22 = 1.5;
+// let b22 = 0.75;
+
+// alert(a22+b22);
+
+// Задание 23 №1
+// let a23_1 = -5;
+
+// alert(a23_1);
+
+// Задание 23 №2
+// let a23_2 = 50;
+
+// alert(-a23_2);
+
+// Задание 25 №1
+// let a25_1 = 13;
+// let b25_1 = 5;
+// alert(a25_1 % b25_1)
+
+// Задание 26 №1
+let a26 = 2;
+alert(a26 ** 10);
