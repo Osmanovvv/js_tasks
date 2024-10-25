17010914d706de23f1b3c7bd20eec146337708d1
Osmanovvv <server.osmanov26@mail.ru>
2024-10-26 02:55:02 +0300

    задача 40

--- Basics/1-41/script_1-41.js
+++ Basics/1-41/script_1-41.js
@@ -173,11 +173,21 @@ dfsdf */
 
 
 // Задание 39 №1
-let num39_1 = 5;
-console.log(num39_1);
+// let num39_1 = 5;
+// console.log(num39_1);
 
 // Задание 39 №2
-let num39_2 = 5;
-let num39_3 = 15;
-let num39_4 = 25;
-console.log(num39_2, num39_3, num39_4);
+// let num39_2 = 5;
+// let num39_3 = 15;
+// let num39_4 = 25;
+// console.log(num39_2, num39_3, num39_4);
+
+
+
+
+// Задание 40 №1
+console.log(10, -5, 0);
+console.log(3.14, -2.5, 1.0);
+console.log('hey');
+console.log(true, false);
+console.log('A', '!', '*');
