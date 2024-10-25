190e1b329efcca83493fb804de5e7a42e2e8fafc
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-10-26 02:01:07 +0300

    задача 29

--- Basics/1-41/script_1-41.js
+++ Basics/1-41/script_1-41.js
@@ -87,8 +87,24 @@ dfsdf */
 
 
 // Задание 28 №1
-let name28 = "Server";
-alert(name28);
+// let name28 = "Server";
+// alert(name28);
 
-let surname28_1 = "Osmanov";
-alert(surname28_1);
+// let surname28_1 = "Osmanov";
+// alert(surname28_1);
+
+
+
+// Задание 29 №1
+let str29 = "!!!";
+alert(str29);
+
+// Задание 29 №2
+let str29_1 = "java";
+let str29_2 = "script";
+alert(str29_1 + str29_2);
+
+// Задание 29 №3
+let str29_3 = "hello";
+let str29_4 = "world";
+alert(str29_3 + ' ' + str29_4);
