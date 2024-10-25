42fcc7843ee5ccff0b9bc49727c9cc3ba9859ce3
Osmanovvv <osmanov.ser.i.23@gmail.com>
2024-10-26 02:17:18 +0300

    задача 31

--- Basics/1-41/script_1-41.js
+++ Basics/1-41/script_1-41.js
@@ -96,23 +96,32 @@ dfsdf */
 
 
 // Задание 29 №1
-let str29 = "!!!";
-alert(str29);
+// let str29 = "!!!";
+// alert(str29);
 
 // Задание 29 №2
-let str29_1 = "java";
-let str29_2 = "script";
-alert(str29_1 + str29_2);
+// let str29_1 = "java";
+// let str29_2 = "script";
+// alert(str29_1 + str29_2);
 
 // Задание 29 №3
-let str29_3 = "hello";
-let str29_4 = "world";
-alert(str29_3 + ' ' + str29_4);
+// let str29_3 = "hello";
+// let str29_4 = "world";
+// alert(str29_3 + ' ' + str29_4);
 
 
 
 
 
 // 30-1
-let str = 'Привет';
-alert(str.length);
+// let str = 'Привет';
+// alert(str.length);
+
+
+
+
+// 31-1
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt = aaa + ${str1} + bbb + ${str2} + ccc
+alert(txt);
