dedf2aac23675690d2b16ec1e4d8bed156830ed8
Osmanovvv <server.osmanov26@mail.ru>
2024-11-02 18:26:21 +0300

    задача 49

--- Basics/42-52/script_42-52.js
+++ Basics/42-52/script_42-52.js
@@ -108,6 +108,19 @@
 
 
 // Задание 48 №1
-let a48 = 5;
-let b48 = 45;
-alert(String(a48) + String(b48)); 
+// let a48 = 5;
+// let b48 = 45;
+// alert(String(a48) + String(b48)); 
+
+
+
+
+// Задание 49 №1
+// let a49_1 = 545;
+// alert(String(a49_1).length);
+
+// Задание 49 №2
+let a49_2 = 55; 
+let b49_2 = 245;   
+let sum49_2 = String(a49_2).length + String(b49_2).length;
+alert(sum49_2);
