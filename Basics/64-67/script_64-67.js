2c783beb6fd2c26c1d2697b245af6cf567fffb8a
Osmanovvv <server.osmanov26@mail.ru>
2024-11-02 21:01:29 +0300

    задача 66

--- Basics/64-67/script_64-67.js
+++ Basics/64-67/script_64-67.js
@@ -17,68 +17,102 @@
 
 
 
+
+
 // Задание 65 №1
-let num65_1 = 1;
-let num65_2 = 2;
-console.log('сумма: ' + (num65_1 + num65_2));
+// let num65_1 = 1;
+// let num65_2 = 2;
+// console.log('сумма: ' + (num65_1 + num65_2));
 
 // Задание 65 №2
-let a65_2 = 1;
-let b65_2 = 2;
-console.log(a65_2 + b65_2);
+// let a65_2 = 1;
+// let b65_2 = 2;
+// console.log(a65_2 + b65_2);
 
 // Задание 65 №3
-let num65_3 = '123';
-let sum65_3 = Number(num65_3[0]) + Number(num65_3[1]) + Number(num65_3[2]);
-console.log(sum65_3);
+// let num65_3 = '123';
+// let sum65_3 = Number(num65_3[0]) + Number(num65_3[1]) + Number(num65_3[2]);
+// console.log(sum65_3);
 
 // Задание 65 №4
-let num65_4 = String(123);
-console.log((num65_4[0]));
+// let num65_4 = String(123);
+// console.log((num65_4[0]));
 
 // Задание 65 №5
-let c65_5 = 0;
-console.log(++c65_5);
+// let c65_5 = 0;
+// console.log(++c65_5);
 
 // Задание 65 №6
-let num65_6 = String(123);
-console.log(num65_6.length);
+// let num65_6 = String(123);
+// console.log(num65_6.length);
 
 // Задание 65 №7
-let k65_7 = 24 * 60 * 60;
-console.log(k65_7);
+// let k65_7 = 24 * 60 * 60;
+// console.log(k65_7);
 
 
 // Задание 65 №8
-let num65_8 = 123;
-let str65_8 = String(num65_8);
-console.log(str65_8.length);
+// let num65_8 = 123;
+// let str65_8 = String(num65_8);
+// console.log(str65_8.length);
 
 
 // Задание 65 №9
-let num65_9 = 123;
-let str65_9 = String(num65_9);
-console.log(str65_9[str65_9.length - 1]);
+// let num65_9 = 123;
+// let str65_9 = String(num65_9);
+// console.log(str65_9[str65_9.length - 1]);
 
 // Задание 65 №10
-let num65_10 = 123;
-let str65_10 = String(num65_10);
-console.log(str65_10.length);
+// let num65_10 = 123;
+// let str65_10 = String(num65_10);
+// console.log(str65_10.length);
 
 // Задание 65 №11
-let num65_11 = 123;
-let str65_11 = String(num65_11);
-console.log(str65_11[str65_11.length - 1]);
+// let num65_11 = 123;
+// let str65_11 = String(num65_11);
+// console.log(str65_11[str65_11.length - 1]);
 
 
 // Задание 65 №12
-let a65_12 = '123';
-let b65_12 = '456';
-let s65_12 = Number(a65_12) + Number(b65_12);
-console.log(s65_12);
+// let a65_12 = '123';
+// let b65_12 = '456';
+// let s65_12 = Number(a65_12) + Number(b65_12);
+// console.log(s65_12);
 
 // Задание 65 №13
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-console.log(aaa + bbb + ccc);
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+// console.log(aaa + bbb + ccc);
+
+
+
+// Задание 66 №1
+alert("Количество секунд в сутках = " + (24 * 60 * 60));
+
+// Задание 66 №2
+alert("Количество секунд в 30 сутках = " + (30 * 24 * 60 * 60));
+
+// Задание 66 №3
+alert("Количество секунд в году = " + (10 * (1024) ** 3));
+
+// Задание 66 №4
+alert("Количество минут в сутках = " + (24 * 60));
+
+// Задание 66 №5
+alert("Количество минут в году = " + (365 * 24 * 60));
+
+// Задание 66 №6
+alert("Количество байт в мегабайте. = " + (1024 * 1024));
+
+// Задание 66 №7
+alert("Количество байт в гигабайте. = " + ((1024) ** 3));
+
+// Задание 66 №8
+alert("Количество байт в 10 гигабайтах. = " + (10 * (1024) ** 3));
+
+// Задание 66 №9
+alert("Количество байт в терабайте. = " + ((1024) ** 4));
+
+// Задание 66 №10
+alert("Количество килобайт в терабайте. = " + ((1024) ** 3));
