8df4d1a9f141c38772da202de3b9a5b10c627260
Osmanovvv <server.osmanov26@mail.ru>
2024-11-02 21:11:29 +0300

    задача 70

--- Arrays/script_68-79.js
+++ Arrays/script_68-79.js
@@ -10,21 +10,33 @@
 
 
 // Задание 69 №1
-let arr69_1 = [1, 2, 3]; 
-console.log(arr69_1[0]); 
-console.log(arr69_1[1]); 
-console.log(arr69_1[2]);
+// let arr69_1 = [1, 2, 3]; 
+// console.log(arr69_1[0]); 
+// console.log(arr69_1[1]); 
+// console.log(arr69_1[2]);
 
 // Задание 69 №2
-let arr69_2 = [1, 2, 3]; 
-console.log(arr69_2[0]); 
-console.log(arr69_2[1]); 
-console.log(arr69_2[2]);
+// let arr69_2 = [1, 2, 3]; 
+// console.log(arr69_2[0]); 
+// console.log(arr69_2[1]); 
+// console.log(arr69_2[2]);
 
 // Задание 69 №3
-let arr69_3 = [1, 2, 3];
-console.log(arr69_3[0] + arr69_3[1] + arr69_3[2]);
+// let arr69_3 = [1, 2, 3];
+// console.log(arr69_3[0] + arr69_3[1] + arr69_3[2]);
 
 // Задание 69 №4
-let arr69_4 = ['a', 'b', 'c', 'd'];
-console.log(String(arr69_4[0])+ "+" + String(arr69_4[1])+ '+' + String(arr69_4[2])+ '+' + String(arr69_4[3]));
+// let arr69_4 = ['a', 'b', 'c', 'd'];
+// console.log(String(arr69_4[0])+ "+" + String(arr69_4[1])+ '+' + String(arr69_4[2])+ '+' + String(arr69_4[3]));
+
+
+
+
+
+// Задание 70 №1
+let arr70_1 = ['disney', 'princess', 'tiana'];
+console.log(arr70_1.length) 
+
+// Задание 70 №2
+let arr70_2 = ['disney', 'princess', 'tiana', 'cartoon'];
+console.log(arr70_2[arr70_2.length - 1]) 
