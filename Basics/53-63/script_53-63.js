8de898fb49647c80e770ce90b6fd8e97806eda55
Osmanovvv <server.osmanov26@mail.ru>
2024-11-02 20:22:04 +0300

    задача 55

--- Basics/53-63/script_53-63.js
+++ Basics/53-63/script_53-63.js
@@ -1,11 +1,18 @@
 // Задание 54 №1
-let str54_1 = 'serverosmanov';
-alert(str54_1[str54_1.length - 1]);
+// let str54_1 = 'serverosmanov';
+// alert(str54_1[str54_1.length - 1]);
 
 // Задание 54 №2
-let str54_2 = 'serverosmanov';
-alert(str54_2[str54_2.length - 2]);
+// let str54_2 = 'serverosmanov';
+// alert(str54_2[str54_2.length - 2]);
 
 // Задание 54 №3
-let str54_3 = 'serverosmanov';
-alert(str54_3[str54_3.length - 3]);
+// let str54_3 = 'serverosmanov';
+// alert(str54_3[str54_3.length - 3]);
+
+
+
+
+// Задание 55 №1
+let test55 = '12345'; 
+alert(Number(test55[0]) + Number(test55[1]) + Number(test55[2]) + Number(test55[3]) + Number(test55[4]));
