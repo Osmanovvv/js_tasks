72420abe288527f16b86dc46b03c67ba40dd3731
Osmanovvv <server.osmanov26@mail.ru>
2024-11-11 10:41:08 +0300

    Задача 115-120

--- Conditions/script_104-142.js
+++ Conditions/script_104-142.js
@@ -273,3 +273,129 @@ if (num113_3 <= 1 && num113_4 >= 3) {
 //   } else {
 //     console.log('---');
 //   }
+
+
+  // Задание 116 №1
+let num116_1 = 3;
+if ((num116_1 > 5 && num116_1 < 10) || num116_1 == 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+} 
+
+// Задание 116 №2
+let num116_2 = 3;		
+if (num116_2 > 5 || (num116_2 > 0 && num116_2 < 3)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 116 №3
+let num116_3 = 3;
+if (num116_3 == 9 || (num116_3 > 10 && num116_3 < 20) || (num116_3 > 20 && num116_3 < 30)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 117 №1
+let num117_1 = 5;
+let num117_2 = 1;
+if (! (num117_1 < 0 || num117_2 > 10) ) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 118 №1
+let test118_1 = true;
+
+if (test118_1 === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 118 №2
+let test118_2 = false;
+
+if (test118_2 === false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 119 №1
+// При приведении к логическому типу (boolean) значения, которые дают false, включают:
+// 1. false
+// 2. 0 
+// 3. -0 
+// 4. NaN 
+// 5. "" — пустая строка.
+// 6. null 
+// 7. undefined 
+
+// Задание 119 №2
+// let test = 1;	
+// if (test == true) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 119 №3
+// let test = 0;  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 119 №4
+// let test = 1;  
+// if (test == false) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 119 №5
+// let test = 1;  
+// if (test != true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 119 №6
+// let test = '';  
+// if (test == false) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 119 №7
+// let test;  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 119 №8
+// let test = 3 * 'a';  
+// if (test == true) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 120 №1
+let test120 = true;
+if (test120) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
