37b8b1d795e29dba9c908bfd620d89aca2e12cb0
Osmanovvv <server.osmanov26@mail.ru>
2024-11-11 10:32:47 +0300

    Задача 104-115

---
+++ Conditions/script_104-142.js
@@ -0,0 +1,275 @@
+// Задание 105 №1
+let test105_1 = 20;
+if (test105_1 > 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №2
+let test105_2 = 5;
+if (test105_2 < 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №3
+let test105_3 = 10;
+if (test105_3 >= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 105 №4
+let test105_4 = 2;
+if (test105_4 <= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 106 №1
+let test106 = 10;
+if (test106 == 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 107 №1
+let test107 = 25;
+if (test107 != 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+// Задание 108 №1
+let test108_1 = 5;
+let test108_2 = 15;
+if (test108_1 > test108_2) {
+	console.log('test108_1 > test108_2'); 
+} else {
+	console.log('test108_1 < test108_2');
+}
+
+// Задание 108 №2
+let test108_3 = 5;
+let test108_4 = 5;
+if (test108_3 == test108_4) {
+	console.log('test108_3 = test108_4'); 
+} else {
+	console.log('test108_3 != test108_4');
+}
+
+// Задание 109 №1
+let test109_1 = 'cinderella';
+let test109_2 = 'disney';
+if (test109_1 == test109_2) {
+	console.log('test109_1 = test109_2'); 
+} else {
+	console.log('test109_1 != test109_2');
+}
+
+// Задание 110 №1
+let test110_1 = '123';
+let test110_2 = 123;
+if (test110_1 == test110_2) {
+	console.log('test110_1 = test110_2'); 
+} else {
+	console.log('test110_1 != test110_2');
+}
+
+// Задание 111 №1
+// let test1 = '3';
+// let test2 = '3';		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 111 №2
+// let test1 = '3';
+// let test2 = '3';	
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 111 №3
+// let test1 = 3;
+// let test2 = '3';		
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 111 №4
+// let test1 = 3;
+// let test2 = '3';	
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 111 №5
+// let test1 = 3;
+// let test2 = 3;		
+// if (test1 === test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 112 №1
+// let test1 = '3';
+// let test2 = '3';
+// if (test1 != test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 112 №2
+// let test1 = '3';
+// let test2 = '3';		
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 112 №3
+// let test1 = 3;
+// let test2 = '3';	
+// if (test1 != test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 112 №4
+// let test1 = 3;
+// let test2 = '3';		
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 112 №5
+// let test1 = 3;
+// let test2 = 2;	
+// if (test1 !== test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 113 №1
+let num113_1 = 4;
+if (num113_1 > 0 && num113_1 < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 113 №2
+let num113_2 = 1;
+if (num113_2 >= 10 && num113_2 <= 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 113 №3
+let num113_3 = 0;
+let num113_4 = 6;
+if (num113_3 <= 1 && num113_4 >= 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// Задание 114 №1
+// let num1 = -10;
+// let num2 = -10;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+ 
+// Задание 114 №2
+// let num1 = 0;
+// let num2 = 0;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+        
+// Задание 114 №3
+// let num1 = 0;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 114 №4
+// let num1 = 5;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 114 №5
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 114 №6
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 114 №7
+// let num = 2;
+// if (num == 0 || num == 1) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 114 №8
+// let num = 2;
+// if (num == 0 || num == 1 || num == 2) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 115 №1
+// if (num == 0 || num > 1 && num < 5) {
+//     console.log('+++');
+//   } else {
+//     console.log('---');
+//   }
