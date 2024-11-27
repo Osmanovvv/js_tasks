38a34eeaefa4b731d99ce64d3b3576b95f4e68c3
Osmanovvv <server.osmanov26@mail.ru>
2024-11-27 08:23:27 +0300

    225-226

--- Types of functions/script_223-239.js
+++ Types of functions/script_223-239.js
@@ -20,3 +20,31 @@ console.log(func224);
 // Задание 224 №4
 func224 = 123; 
 console.log(func224); 
+
+// Задание 225 №1
+console.log('Результат задания 225:');
+// Задание 1
+function func225_1() {
+	return 3;
+}
+  
+// Задание 225 №2
+ const func225_2 = func225_1;
+
+// Задание 225 №3
+console.log(func225_1() + func225_2()); 
+
+// Задание 226 №1
+console.log('Результат задания 226:');
+let func226_1 = function() {
+	return 1;
+  };
+
+// Задание 226 №2
+let func226_2 = function() {
+	return 2;
+  };
+
+// Задание 226 №3
+alert('Результат задания 226: ' + (func226_1() + func226_2()))
+console.log(func226_1() + func226_2())
