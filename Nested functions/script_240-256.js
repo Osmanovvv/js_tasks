263f0ebbd9ba9775464fc6538234ab3cdb9d6890
Osmanovvv <server.osmanov26@mail.ru>
2024-11-27 08:35:42 +0300

    248

--- Nested functions/script_240-256.js
+++ Nested functions/script_240-256.js
@@ -183,3 +183,64 @@ function func245(num1, num2) {
 // 	func();
 // }
 // test(1, 2); 
+
+
+// Задание 248 №1
+// function test(num) {
+// 	function func(localNum) {
+// 		console.log(localNum);
+// 	}
+// 	func(num);
+// }
+// test(1); 
+
+// Задание 248 №2
+// function test(num) {
+// 	function func(localNum) {
+// 		console.log(localNum);
+// 	}
+// 	func(num + 1);
+// }
+// test(1); 
+
+// Задание 248 №3
+// function test(num) {
+// 	function func(localNum) {
+// 		console.log(num);
+// 	}
+	
+// 	func(num + 1);
+// }
+// test(1); 
+
+// Задание 248 №4
+// function test(num) {
+// 	function func(localNum) {
+// 		localNum = 2;
+// 	}
+	
+// 	func(num);
+// 	console.log(num);
+// }
+
+// test(1); 
+
+// Задание 248 №5
+// function test(num) {
+// 	function func(localNum) {
+// 		localNum = 2;
+// 	}
+// 	func(num);
+// 	console.log(localNum); 
+// }
+// test(1); 
+
+// Задание 248 №6
+// function test(num) {
+// 	function func(localNum) {
+// 		num = 2;
+// 	}
+// 	func(num);
+// 	console.log(num);
+// }
+// test(1); 
