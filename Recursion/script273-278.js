be26db1a08c50c8e2cc75b81c3fe05e3b628aefa
Osmanovvv <server.osmanov26@mail.ru>
2024-11-30 16:38:32 +0300

    275

--- Recursion/script273-278.js
+++ Recursion/script273-278.js
@@ -8,3 +8,15 @@ function func(arr274_1, index) {
 	}
 }
 func(arr274_1, 0);
+
+// Задание 275 №1
+console.log('Результат задачи 275 №1:');
+function task275(arr275_1) {
+	let sum275_1 = arr275_1[0] * arr275_1[0];
+	if (arr275_1.length !== 1) {
+	  sum275_1 += task275(arr275_1.slice(1)); 
+	}
+	return sum275_1;
+  }
+  let arr275_1 = [1, 2, 3, 4, 5];
+  console.log(task275(arr275_1));
